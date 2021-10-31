4d9af17af4048633360e122a8e877a55f148edfb
Kartik <9415kks@gmail.com>
2021-10-31 23:01:26 +0530

    Add the homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import "./app.css";
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div>
+      <Topbar/>
+      <div className="container">
+        <Sidebar/>
+        <div className="others">other pages</div>
+      </div>
     </div>
   );
 }
