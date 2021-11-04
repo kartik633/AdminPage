7bf660e0fb2e2529e24bbc55f6a5c10fd37f02d2
Kartik <9415kks@gmail.com>
2021-11-04 17:26:41 +0530

    Added Recharts

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+    {
+        name: 'Jan',
+        "Active User": 4000,
+    },
+    {
+        name: 'Feb',
+        "Active User": 4300,
+    },
+    {
+        name: 'Mar',
+        "Active User": 2400,
+    },
+    {
+        name: 'Apr',
+        "Active User": 7500,
+    },
+    {
+        name: 'May',
+        "Active User": 3500,
+    },
+    {
+        name: 'Jun',
+        "Active User": 1200,
+    },
+    {
+        name: 'Jul',
+        "Active User": 7500,
+    },
+    {
+        name: 'Aug',
+        "Active User": 5700,
+    },
+    {
+        name: 'Sep',
+        "Active User": 9700,
+    },
+    {
+        name: 'Oct',
+        "Active User": 4780,
+    },
+    {
+        name: 'Nov',
+        "Active User": 6760,
+    },
+    {
+        name: 'Dec',
+        "Active User": 9970,
+    },
+  ];
